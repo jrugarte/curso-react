df49f72bc5f0d3cbdc6d2b6fc71d13d3c040717c
Junior Ugarte <jr.ugartee@gmail.com>
2023-07-25 16:49:16 -0600

    changes in loading

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -25,20 +25,26 @@ function AppUI({
   return (
     <React.Fragment>
       {/* En los componentes, podemos pasarle props como si fueran atributos, para que funcionen como parametros en cada funcion, como en el siguiente caso:  */}
-      <TodoCounter completedTodos={completedTodos} totalTodos={totalTodos} />
+      {/* <TodoCounter completedTodos={completedTodos} totalTodos={totalTodos} /> */}
       {loading && (
         <React.Fragment>
           <CounterLoading />
         </React.Fragment>
       )}
       {!loading && searchedTodos.length === 0 && <EmptyCounter />}
+      {!loading && searchedTodos.length > 0 && (
+        <TodoCounter completedTodos={completedTodos} totalTodos={totalTodos} />
+      )}
 
       {/* en la siguiente linea declaro las propiedades y le asigno el estado y el actualizador(setState) del estado a las mismas para despues pasarla como props al componente hijo (TodoInput) */}
-      <TodoInput searchValue={searchValue} setSearchValue={setSearchValue} />
+      {/* <TodoInput searchValue={searchValue} setSearchValue={setSearchValue} /> */}
       {loading && (
         <React.Fragment>
           <InputLoading />
         </React.Fragment>
+      )}
+      {!loading && (
+        <TodoInput searchValue={searchValue} setSearchValue={setSearchValue} />
       )}
 
       <TodoList>
--- src/inputloading/inputloading.js
+++ src/inputloading/inputloading.js
@@ -1,7 +1,12 @@
+import "./inputloading.css";
 import React from "react";
 
 function InputLoading() {
-  return <div className="filter-loading"></div>;
+  return (
+    <div className="filter-loading">
+      <input />
+    </div>
+  );
 }
 
 export { InputLoading };
